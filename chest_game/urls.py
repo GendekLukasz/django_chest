e9fbc9c35a5d3eb9e2a61ab3f849f67d8e32d89c
lukasz <gendeklukasz@gmail.com>
2019-12-27 17:35:13 +0100

    add admin view

--- chest_game/urls.py
+++ chest_game/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.home, name='chest-play'),
+    path('admin/', admin.site.urls),
 ]
--- chest_project/urls.py
+++ chest_project/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('play/', include('chest_game.urls')),
+    path('admin/', admin.site.urls),
 ]
